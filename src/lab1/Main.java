b7e610ebfbd8e258eb3b8845d6d96df74914629a
butermorg <badcawa@mail.ru>
2020-09-10 00:07:34 +0800

    this is test commit

---
+++ src/lab1/Main.java
@@ -0,0 +1,16 @@
+package lab1;
+public class Main {
+    public static void main(String[] args) {
+    Input in = new Input();
+    //lab_1.3
+    /*Вызов метода "Ввод строки"*/
+        in.Inp();
+    /*Вызов метода "Проверка пароля"*/
+        in.Ver();
+    //lab_1.4
+    /*Вызов метода "Ввод и сумма двух строк"*/
+        in.Sum();
+    /*Вызов метода "Вывод результата"*/
+        in.Result();
+    }   
+}
